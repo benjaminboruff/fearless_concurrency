47c76e3d6fb3ec88b09307cf8125b7429c0953b4
Benjamin H Boruff <11185240+benjaminboruff@users.noreply.github.com>
2022-12-20 16:17:38 -0500

    sleeping and waking threads

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi from number {} in the spawned thread", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    handle.join().unwrap();
+
+    for i in 1..5 {
+        println!("hi from number {} in the main thread", i);
+        thread::sleep(Duration::from_millis(1));
+    }
 }
